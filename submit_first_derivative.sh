19072f1cb6e9cd82cae4df2809f5d474a7416568
Erik Wang <erikwang@college.harvard.edu>
2024-07-19 14:44:57 -0400

    First derivative one layer CNN training

---
+++ submit_first_derivative.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --partition=serc
+#SBATCH --job-name=test1a
+#SBATCH --output=test_%A.out  
+#SBATCH --error=test_%A.out 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=2
+#SBATCH --time=8:00:00
+#SBATCH --gpus=1
+#SBATCH --mem-per-cpu=10G
+
+module load python/3.9.0
+
+source ../venvs/sciml3/bin/activate
+python3 first_derivative_one_layer.py
