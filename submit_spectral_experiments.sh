7104dd34671f824260f131cc143cd9dae730e5b4
Erik Wang <erikwang@college.harvard.edu>
2024-08-31 10:35:14 -0400

    Update submit script

--- submit_spectral_experiments.sh
+++ submit_spectral_experiments.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --partition=serc
-#SBATCH --job-name=test_layer_3runs
+#SBATCH --job-name=layer_runs
 #SBATCH --output=test_layer_runs2%A.out  
 #SBATCH --error=test_%A.out 
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --time=72:00:00
+#SBATCH --time=120:00:00
 #SBATCH --gpus=1
 #SBATCH --constraint=GPU_SKU:A100_SXM4
 #SBATCH --mem-per-cpu=10G
